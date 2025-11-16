3a4ca7495d844d7013dd72b9be57e915a884b930
alexandrahopia <bhm558@myy.haaga-helia.fi>
2025-11-16 21:22:53 +0200

    Add vko3 React build

---
+++ vko3-react/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Viikko 3 – Vite + React</h1>
+      <p>Tämä on minun Vite + React -sovellus tälle kurssille.</p>
+    </div>
+  );
+}
+
+export default App;
+
