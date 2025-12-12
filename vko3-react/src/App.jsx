187401edd3b34fb5f6443f45b2e791ccf43e3c41
alexandrahopia <bhm558@myy.haaga-helia.fi>
2025-12-12 18:26:38 +0200

    Viikko 6 julkaistu

--- vko3-react/src/App.jsx
+++ vko3-react/src/App.jsx
@@ -1,16 +1,24 @@
 import LoginForm from "./LoginForm";
+import MatomoStats from "./MatomoStats";
 import "./App.css";
 
 function App() {
   return (
     <div className="app-root">
-      <h1 className="app-title">Viikko 4 – Firebase Login</h1>
+      <h1 className="app-title">Viikko 6 – Matomo API</h1>
+
+      <h2>Firebase Login (viikko 4)</h2>
       <LoginForm />
+
+      <hr />
+
+      <MatomoStats />
     </div>
   );
 }
 
 export default App;
+
 
 
 
